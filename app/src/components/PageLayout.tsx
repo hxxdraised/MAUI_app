2f99c378cfbf74f48d135c5702ce5de651bfab4b
Murat Miftakhov <murmif02@gmail.com>
2024-04-13 17:02:46 +0300

    page layout background with theme

--- app/src/components/PageLayout.tsx
+++ app/src/components/PageLayout.tsx
@@ -1,11 +1,14 @@
 import React, {PropsWithChildren} from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
+import {useTheme} from 'react-native-paper';
 
 function PageLayout({children}: PropsWithChildren): React.JSX.Element {
+  const theme = useTheme();
   return (
     <SafeAreaView
       style={{
         ...StyleSheet.absoluteFillObject,
+        backgroundColor: theme.colors.background,
       }}>
       {children}
     </SafeAreaView>
